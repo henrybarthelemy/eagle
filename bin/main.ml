454a9c6164e60d747b426130d1b98a4f0854ff6a
henrybarthelemy <henry.v.barthelemy@gmail.com>
2024-10-29 13:42:49 -0400

    Reading from owl file

--- bin/main.ml
+++ bin/main.ml
@@ -4,18 +4,18 @@ open Parser
 open Lexer
 open Ast
 
-
 let parse_expr = Lexer.from_string Parser.program
 
 let print_expr code =
   parse_expr code |> Format.printf "%a\n%!" Ast.pp_program
 
+let read_whole_file filename =
+  (* open_in_bin works correctly on Unix and Windows *)
+  let ch = open_in_bin filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
 
-let () = print_expr "locality alice \n locality bob" 
+let code = read_whole_file "/Users/henrybarthelemy/eagle/bin/owlcode/example1.owl"
 
-
-(*
-let print_expr code =
-  parse_expr code |> Option.get |> Format.printf "%a\n%!" Expr.pp_expr
-
-let () = print_expr "locality alice" *)
+let () = print_expr code
